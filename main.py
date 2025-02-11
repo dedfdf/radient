dc10b3033ccd8b3d3ecb1ef720b8c2280a7bee87
yandex-2023 <ssishunkin@gmail.com>
2025-02-11 16:49:22 +0300

    Change Mashtab

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import requests
 import sys
 import os
+from PyQt6 import QtCore
+from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel
 from PyQt6.QtGui import QPixmap
+
 
 api_key = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
 geo_api_key = '40d1649f-0493-4b70-98ba-98533de7710b'
@@ -10,12 +13,16 @@ geo_api_key = '40d1649f-0493-4b70-98ba-98533de7710b'
 
 class Window(QWidget):
     def __init__(self):
+        self.z = 10
+        self.params = {'ll': '37.617698,55.755864',
+                  'size': '600,450',
+                  'z': f'{self.z}'}
         super().__init__()
         self.get_image()
         self.initUI()
 
     def get_image(self):
-        response = requests.get(f'http://static-maps.yandex.ru/v1?apikey={api_key}&ll=37.617698,55.755864&lang=ru_RU&size=600,450&z=10')
+        response = requests.get(f'http://static-maps.yandex.ru/v1?apikey={api_key}', params=self.params)
         if response:
             self.map_file = 'map.png'
             with open(self.map_file, 'wb') as file:
@@ -33,8 +40,27 @@ class Window(QWidget):
         self.image.resize(500, 500)
         self.image.setPixmap(self.pixmap)
 
-    def closeEvent(self, event):
-        os.remove(self.map_file)
+    def keyPressEvent(self, event):
+        print(self.z)
+        if self.z + 1 != 21:
+            if event.key() == Qt.Key.Key_Up:
+                self.z += 1
+                self.params = {'ll': '37.617698,55.755864',
+                          'size': '600,450',
+                          'z': f'{self.z}'}
+                self.get_image()
+                self.pixmap = QPixmap(self.map_file)
+                self.image.setPixmap(self.pixmap)
+        if self.z - 1 != 0:
+            if event.key() == Qt.Key.Key_Down:
+                self.z -= 1
+                self.params = {'ll': '37.617698,55.755864',
+                          'size': '600,450',
+                          'z': f'{self.z}'}
+                self.get_image()
+                self.pixmap = QPixmap(self.map_file)
+                self.image.setPixmap(self.pixmap)
+
 
 
 if __name__ == '__main__':
