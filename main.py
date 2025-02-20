9b19dc581ede7c628588ea06f8ee9c99b8921955
Pashtet <pavel.pivikov2008@yandex.ru>
2025-02-20 16:46:17 +0300

    fix arrow

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import sys
 import os
 from PyQt6 import QtCore
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import Qt, QEvent
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QLineEdit, QRadioButton
 from PyQt6.QtGui import QPixmap
 
@@ -59,16 +59,19 @@ class Window(QWidget):
         self.request_field.setPlaceholderText('Введите адрес')
         self.request_field.move(0, 480)
         self.request_field.resize(300, 35)
+        self.request_field.installEventFilter(self)
 
         self.request_button = QPushButton('Искать', self)
         self.request_button.move(310, 480)
         self.request_button.resize(70, 35)
         self.request_button.clicked.connect(self.new_adres)
+        self.request_button.installEventFilter(self)
 
         self.reset = QPushButton('Сброс', self)
         self.reset.move(390, 480)
         self.reset.resize(70, 35)
         self.reset.clicked.connect(self.sbros)
+        self.reset.installEventFilter(self)
 
         self.adres_field = QLineEdit(self)
         self.adres_field.setPlaceholderText('Адрес найденного объекта')
@@ -88,6 +91,17 @@ class Window(QWidget):
         self.off_pocht_adres = QRadioButton('Выключить отображение почтового индекса', self)
         self.off_pocht_adres.move(0, 645)
         self.off_pocht_adres.clicked.connect(self.display_pocht_adres_field)
+        self.on_pocht_adres.installEventFilter(self)
+        self.off_pocht_adres.installEventFilter(self)
+
+
+    def eventFilter(self, source, event):
+        if event.type() == QEvent.Type.KeyPress:
+            if (event.key() == Qt.Key.Key_Down or event.key() == Qt.Key.Key_Left or
+                    event.key() == Qt.Key.Key_Up or event.key() == Qt.Key.Key_Right):
+                self.check_arrow(event)
+                self.update()
+        return super().eventFilter(source, event)
 
 
     def display_pocht_adres_field(self):
@@ -155,14 +169,7 @@ class Window(QWidget):
         self.theme = "dark" if self.theme == 'light' else "light"
         self.update()
 
-    def keyPressEvent(self, event):
-        if self.z + 1 != 21:
-            if event.key() == Qt.Key.Key_PageUp:
-                self.z += 1
-        if self.z - 1 != 0:
-            if event.key() == Qt.Key.Key_PageDown:
-                self.z -= 1
-
+    def check_arrow(self, event):
         if self.z <= 21 and self.z >= 17:
             k = self.z / 100000
         elif self.z < 17 and self.z >= 14:
@@ -173,7 +180,6 @@ class Window(QWidget):
             k = self.z / 20
         else:
             k = self.z * 2
-
         if event.key() == Qt.Key.Key_Down:
             if float(self.ll[1]) - k >= -84.77659799999994:
                 self.ll[1] = str(float(self.ll[1]) - k)
@@ -186,6 +192,16 @@ class Window(QWidget):
         elif event.key() == Qt.Key.Key_Right:
             if float(self.ll[0]) + k <= 180:
                 self.ll[0] = str(float(self.ll[0]) + k)
+
+
+    def keyPressEvent(self, event):
+        if self.z + 1 != 21:
+            if event.key() == Qt.Key.Key_PageUp:
+                self.z += 1
+        if self.z - 1 != 0:
+            if event.key() == Qt.Key.Key_PageDown:
+                self.z -= 1
+        self.check_arrow(event)
         self.update()
 
 
